93adb3f6692e547270d8f699d4ff90fc56e9a28f
simonotM <simonot@iut.univ-paris8.fr>
2023-05-16 10:57:01 +0200

    Produit.java

---
+++ MagasinA/src/Produit.java
@@ -0,0 +1,32 @@
+public class Produit {
+    private String libelle;
+    private int prix;
+
+    public Produit(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "libelle='" + libelle + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
+}
