51e1357f3930837daf78c1c6212c910c97c5279a
simonotM <simonot@iut.univ-paris8.fr>
2023-05-18 14:12:11 +0200

    3 e modif de main par A (conflit)

--- MagasinA/src/Main.java
+++ MagasinA/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ajout A");
-        System.out.println("Hello world!");
+        System.out.println("Hello world! de A");
         System.out.println("coucou");
         System.out.println("ajout etudiant A");
     }
