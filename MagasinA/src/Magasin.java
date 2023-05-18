9c69031fcdc0cd33f680ca169228115990ac98ad
simonotM <simonot@iut.univ-paris8.fr>
2023-05-18 20:45:25 +0200

    commit de A sur branche dev

--- MagasinA/src/Magasin.java
+++ MagasinA/src/Magasin.java
@@ -2,4 +2,12 @@ import java.util.ArrayList;
 
 public class Magasin {
     private ArrayList<Produit> stock;
+
+    public Magasin(ArrayList<Produit> stock) {
+        this.stock = stock;
+    }
+
+    public ArrayList<Produit> getStock() {
+        return stock;
+    }
 }
