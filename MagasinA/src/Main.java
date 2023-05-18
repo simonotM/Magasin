60d94888f042c34855fb492628273f5302974971
simonotM <simonot@iut.univ-paris8.fr>
2023-05-18 14:04:37 +0200

    Bis change main

--- MagasinA/src/Main.java
+++ MagasinA/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("coucou");
         System.out.println("ajout etudiant A");
+        System.out.println("ajout B");
     }
 }
