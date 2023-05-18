aaf77789c71d6f863a6d69b69397f979b8c33413
simonotM <simonot@iut.univ-paris8.fr>
2023-05-18 13:57:15 +0200

    Bis change Produit

--- MagasinA/src/Produit.java
+++ MagasinA/src/Produit.java
@@ -26,6 +26,7 @@ public class Produit {
     public String toString() {
         return "Produit" +
                 "libelle='" + libelle + '\'' +
-                ", prix=" + prix ;
+                ", prix=" + prix
+                ;
     }
 }
